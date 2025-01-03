8894144cc6e8f56456839505e3a7f922534babe1
Bill Collins <bill.collins@hp.com>
2025-01-03 14:32:09 +0000

    eg

---
+++ packages/demo/src/index.js
@@ -0,0 +1,7 @@
+import classes from 'vcm-repro-styles';
+
+document.querySelector('#app').innerHTML = `
+  <div class="${classes.root}">
+    Hello world
+  </div>
+`;
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import classes from './styles.module.css';
+
+document.querySelector('#app').innerHTML = `
+  <div class="${classes.root}">
+    Hello world
+  </div>
+`;
